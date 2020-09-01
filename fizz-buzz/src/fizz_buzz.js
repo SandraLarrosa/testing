7d07b7a71f6a2d3b72ed61f1367dd8fa11143090
Alexandra <sandra.larrosa88@gmail.com>
2020-09-01 16:40:06 +0200

    return number between 0 and 100

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -1,5 +1,7 @@
 class FizzBuzz {
-
+    getNumbers () {
+        return 1;
+    }
 }
 
 module.exports.FizzBuzz = FizzBuzz;
--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -1,9 +1,11 @@
-let {FizzBuzz} = require('../src/fizz_buzz');
+let { FizzBuzz } = require('../src/fizz_buzz');
 
-describe('FizzBuzz', function() {
-  it('change_this_name', function() {
-    let fizzBuzz = new FizzBuzz();
+describe('FizzBuzz', function () {
+  it('return number between 0 and 100', function () {
+    let fizzBuzz = new FizzBuzz(1);
 
-    expect(true).toBe(true);
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers).toBe(1);
   });
 });
