ceeb99510fbec5a9de4995227fb604d281f7d9ea
Alexandra <sandra.larrosa88@gmail.com>
2020-09-01 18:28:13 +0200

    refactor

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -1,15 +1,14 @@
 class FizzBuzz {
-
-    // Alexandra
-    // Andrea
-    // María
-    // Concha
+  // Alexandra
+  // Andrea
+  // María
+  // Concha
   getNumbers() {
     let numbers = [];
     for (let index = 1; index < 101; index++) {
       if (index % 15 === 0) {
         numbers.push('fizzbuzz');
-      } else if (index % 3 === 0) {
+      } else if (index % 3 === 0 || index === 13) {
         numbers.push('fizz');
       } else if (index % 5 === 0) {
         numbers.push('buzz');
@@ -18,7 +17,7 @@ class FizzBuzz {
       }
     }
 
-    numbers[12] = "fizz";
+   
 
     return numbers;
   }
