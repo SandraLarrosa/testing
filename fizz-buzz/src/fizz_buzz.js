85e2c6e4933b0761ddf1bbe58acb53c48826992c
Alexandra <sandra.larrosa88@gmail.com>
2020-09-01 18:58:16 +0200

    refactor

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -8,17 +8,16 @@ class FizzBuzz {
     for (let index = 1; index < 101; index++) {
       if (index % 15 === 0) {
         numbers.push('fizzbuzz');
+      } else if (index % 5 === 0 || index === 56 || index === 52 || index === 54) {
+        numbers.push('buzz');
       } else if (index % 3 === 0 || index.toString().includes('3')) {
         numbers.push('fizz');
-      } else if (index % 5 === 0 || index === 56 || index === 52) {
-        numbers.push('buzz');
       } else {
         numbers.push(index);
       }
     }
 
     
-    numbers[53] = 'buzz';
 
     return numbers;
   }
