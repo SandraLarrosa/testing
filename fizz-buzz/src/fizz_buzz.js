07744711f4ab5468b54ebe7d868ec6116c7fe489
Alexandra <sandra.larrosa88@gmail.com>
2020-09-01 19:43:55 +0200

    refactor

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -6,24 +6,27 @@ class FizzBuzz {
   getNumbers() {
     let numbers = [];
     for (let index = 1; index < 101; index++) {
-      let fizzValue = '';
-      if (
-        index % 15 === 0 ||
-        (this.contain5(index) && this.contain3(index)) ||
-        (index % 3 === 0 && this.contain5(index))
-      ) {
-        fizzValue = 'fizzbuzz';
-      } else if (index % 5 === 0 || this.contain5(index)) {
-        fizzValue = 'buzz';
-      } else if (index % 3 === 0 || this.contain3(index)) {
-        fizzValue = 'fizz';
-      } else {
-        fizzValue = index;
-      }
+      const fizzValue = this.calculateFizzValue(index);
       numbers.push(fizzValue);
     }
 
     return numbers;
+  }
+
+  calculateFizzValue(index) {
+    if (
+      index % 15 === 0 ||
+      (this.contain5(index) && this.contain3(index)) ||
+      (index % 3 === 0 && this.contain5(index))
+    ) {
+      return 'fizzbuzz';
+    } else if (index % 5 === 0 || this.contain5(index)) {
+      return 'buzz';
+    } else if (index % 3 === 0 || this.contain3(index)) {
+      return 'fizz';
+    } else {
+      return index;
+    }
   }
 
   contain5(index) {
